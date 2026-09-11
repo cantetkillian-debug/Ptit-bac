fb5d7422f04f0b61a8627d9f1bc210153573258a
Milce <cantetkillian@gmail.com>
2026-09-11 14:43:53 +0200

    Add files via upload

--- economy-hook.js
+++ economy-hook.js
@@ -21,7 +21,7 @@ const originalLoader = Module._extensions[".js"];
 
 function need(source, search, replacement, label) {
   if (!source.includes(search)) {
-    throw new Error("[Economy V2.2] Patch introuvable: " + label);
+    throw new Error("[Economy V2.3] Patch introuvable: " + label);
   }
   return source.replace(search, replacement);
 }
@@ -29,7 +29,7 @@ function need(source, search, replacement, label) {
 function needRegex(source, regex, replacement, label) {
   regex.lastIndex = 0;
   if (!regex.test(source)) {
-    throw new Error("[Economy V2.2] Patch introuvable: " + label);
+    throw new Error("[Economy V2.3] Patch introuvable: " + label);
   }
   regex.lastIndex = 0;
   return source.replace(regex, replacement);
@@ -93,7 +93,7 @@ async function ensureEconomySchema() {
     await pgPool.query('CREATE INDEX IF NOT EXISTS economy_transactions_wallet_idx ON public.economy_transactions(wallet_token, created_at DESC)');
 
     economySchemaReady = true;
-    console.log("Economie V2.2 active: 50 pieces, 5 vies, recharge 30 min.");
+    console.log("Economie V2.3 active: 50 pieces, 5 vies, recharge 30 min.");
   })().catch(err => {
     economySchemaPromise = null;
     throw err;
@@ -451,13 +451,13 @@ Module._extensions[".js"] = function patchedLoader(mod, filename) {
     return originalLoader(mod, filename);
   }
 
-  console.log("[Economy V2.2] patch de:", filename);
+  console.log("[Economy V2.3] patch de:", filename);
   let source = fs.readFileSync(filename, "utf8");
 
   source = need(
     source,
     'const GAME_COST = 5;',
-    'const GAME_COST = 0; // Economie V2.2: entree payee en vies',
+    'const GAME_COST = 0; // Economie V2.3: entree payee en vies',
     "GAME_COST"
   );
 
@@ -637,12 +637,12 @@ function distributeRewards`,
   // IMPORTANT: vrai retour à la ligne, pas les caractères "\\n".
   source = need(
     source,
-    '  io.on("connection", socket => {',
-    '  io.on("connection", socket => {\n' + SOCKETS,
+    'io.on("connection", socket => {',
+    'io.on("connection", socket => {\n' + SOCKETS,
     "economy sockets"
   );
 
   mod._compile(source, filename);
 };
 
-console.log("[Economy V2.2] runtime patch charge.");
+console.log("[Economy V2.3] runtime patch charge.");
